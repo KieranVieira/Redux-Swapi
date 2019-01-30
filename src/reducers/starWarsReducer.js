cdfb73aa822292f54d48dca99d541404fff51ae0
KieranVieira <kieranvieira@live.com>
2019-01-30 15:35:40 -0500

    setup actions and reducer structure

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,33 @@
-import /* we need our action types here*/ "../actions";
+import { 
+  FETCH_CHARACTER_START, 
+  FETCH_CHARACTER_SUCCESS, 
+  FETCH_CHARACTER_FAILURE 
+} from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
   // Array characters, Boolean fetching, null error.
+  isFetching: false,
+  error: null,
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHARACTER_START:
+      return{
+
+      }
+    case FETCH_CHARACTER_SUCCESS:
+      return{
+        
+      }
+    case FETCH_CHARACTER_FAILURE:
+      return {
+        
+      }
     default:
       return state;
   }
